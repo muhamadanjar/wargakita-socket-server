57d26d9cc25acceee868e037bca2be334b6a05df
Muhamad Anjar <arvanria@gmail.com>
2020-07-01 09:43:16 +0700

    add route sample

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server); // < Interesting!
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+server.listen(port, () => console.log(`Listening on port ${port}`));
 
